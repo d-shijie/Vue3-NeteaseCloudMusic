8a2b7b4cdb83ea694805e7163b949fbccc951440
dshijie <943996999@qq.com>
2023-05-05 15:45:20 +0800

    chore: 删除部分多余代码

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: '/index'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/index',
+      name: 'Index',
+      component: () => import('@/views/appIndex/appIndex.vue')
     }
   ]
 })
---
+++ src/views/appIndex/appIndex.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>111</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style scoped></style>
