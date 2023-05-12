88ba6d18442b6d6a4d6e547fe9588a9b5440a0e7
dshijie <943996999@qq.com>
2023-05-12 10:09:49 +0800

    feat: 二维码生成

--- src/components/LoginCard/loginCard.vue
+++ src/components/LoginCard/loginCard.vue
@@ -1,7 +1,35 @@
 <template>
-  <div class="login-card">1111</div>
+  <div class="login-card">
+    <div class="qrcode">
+      <h1>扫码登录</h1>
+      <qrcode-vue :value="qrcodeUrl" :size='170'></qrcode-vue>
+      <p>使用<a href="https://music.163.com/#/download">网易云音乐APP</a> 扫码登录</p>
+      <a>选择其他登录方式 ></a>
+    </div>
+
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import QrcodeVue from 'qrcode.vue'
+import { ref } from 'vue';
+const qrcodeUrl = ref('www.baidu.com')
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.qrcode {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    font-weight: normal;
+  }
+
+  a {
+    cursor: pointer;
+    margin-top: 100px;
+  }
+}
+</style>
