94cfdc3dbbd369907b5bdd716c8b234c3a8a00fc
dshijie <943996999@qq.com>
2023-05-26 17:43:33 +0800

    feat: 节目详情信息

--- src/views/djDetail/djDetailIndex.vue
+++ src/views/djDetail/djDetailIndex.vue
@@ -1,13 +1,48 @@
 <template>
   <div class="dj-detail">
     <DjDetailHeader />
-    <DjDetailPrograms />
+    <div class="tabs">
+      <div @click="activeTab = 'detail'" class="tab" :class="{ 'active': activeTab === 'detail' }">详情</div>
+      <div @click="activeTab = 'programs'" class="tab" :class="{ 'active': activeTab === 'programs' }">声音</div>
+      <div @click="activeTab = 'recommend'" class="tab" :class="{ 'active': activeTab === 'recommend' }">推荐</div>
+    </div>
+    <DjDetailInfo v-if="activeTab === 'detail'" />
+    <DjDetailPrograms v-if="activeTab === 'programs'" />
+    <DjDetailRecommend v-if="activeTab === 'recommend'" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import DjDetailHeader from './djDetailHeader.vue';
 import DjDetailPrograms from './djDetailPrograms.vue';
+import DjDetailInfo from './djDetailInfo.vue'
+import DjDetailRecommend from './djDetailRcommend.vue'
+const activeTab = ref<'detail' | 'programs' | 'recommend'>('programs')
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.tabs {
+  display: flex;
+  font-size: 14px;
+  padding: 0 30px 20px;
+
+  .tab {
+    margin-right: 24px;
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+
+    &:hover {
+      color: #fff;
+    }
+  }
+
+  .active {
+    padding: 3px 0;
+    font-size: 18px;
+    font-weight: 600;
+    border-bottom: 3px solid rgb(236, 65, 65)
+  }
+}
+</style>
---
+++ src/views/djDetail/djDetailInfo.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="dj-detail-info">
+    <div class="title">
+      电台内容简介
+    </div>
+    <p>{{ detail.desc }}</p>
+    <div class="title">
+      作者简介
+    </div>
+    <p></p>
+    <div class="title">
+      主播简介
+    </div>
+    <p>{{ detail.dj.description }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { getDjDetailApi } from '@/api/dj';
+import { useRoute } from 'vue-router';
+const route = useRoute()
+const detail = ref()
+const getDjDetail = () => {
+  getDjDetailApi(Number(route.query.id)).then(res => {
+    detail.value = res.data.data
+  })
+}
+getDjDetail()
+</script>
+
+<style scoped lang="scss">
+.dj-detail-info {
+  padding: 10px 30px;
+
+  .title {
+
+    margin-bottom: 12px;
+    text-indent: 12px;
+    line-height: 20px;
+    font-size: 15px;
+    font-weight: 600;
+    position: relative;
+
+    &::before {
+      content: '';
+      width: 2px;
+      height: 80%;
+      background-color: rgb(236, 65, 65);
+      display: inline-block;
+      position: absolute;
+      left: 0;
+      bottom: 4px;
+    }
+  }
+
+  p {
+    font-size: 13px;
+    color: var(--v-m-text-dark-color);
+    margin-bottom: 24px;
+  }
+}
+</style>
