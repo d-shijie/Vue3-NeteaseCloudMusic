e5f58afd5a0497a9403e91a10d427d257de54f3f
dshijie <943996999@qq.com>
2023-05-31 18:34:14 +0800

    feat: 添加楼层评论接口

--- src/api/comment.ts
+++ src/api/comment.ts
@@ -1,10 +1,10 @@
 import { request } from '@/util/request'
-
+// 0: 歌曲 1: mv  2: 歌单 3: 专辑 4: 电台 5: 视频 6: 动态
+type CType = 0 | 1 | 2 | 3 | 4 | 5 | 6
 interface CommentType {
   // 1 发送, 2 回复
   t: 1 | 2
-  // 0: 歌曲 1: mv  2: 歌单 3: 专辑 4: 电台 5: 视频 6: 动态
-  type: 0 | 1 | 2 | 3 | 4 | 5 | 6
+  type: CType
   id: number
   content: string
   commentId?: number
@@ -14,5 +14,20 @@ export function commentApi (data: CommentType) {
     url: "/comment",
     method: 'post',
     data
+  })
+}
+
+// 楼层评论
+interface FloorCommentType {
+  parentCommentId: number
+  id: number
+  type: CType
+  limit?: number
+}
+export function getFloorCommentApi (params: FloorCommentType) {
+  return request({
+    url: "/comment/floor",
+    method: 'get',
+    params
   })
 }
