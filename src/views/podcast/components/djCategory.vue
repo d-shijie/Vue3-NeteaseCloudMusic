3a633b35cdcd1569b3db11af8a22ea9a537fb2a0
dshijie <943996999@qq.com>
2023-07-20 14:59:13 +0800

    perf: 播客分类slider根据传入的rows和column参数进行grid布局

--- src/views/podcast/components/djCategory.vue
+++ src/views/podcast/components/djCategory.vue
@@ -1,23 +1,30 @@
 <template>
   <div class="h-100% flex">
     <div class="flex w-20px items-center mx-8px cursor-pointer">
-      <svg-icon v-show="currentIndex!==0" @click="prev" style="font-size: 20px;" name="left"></svg-icon>
+      <svg-icon v-show="currentIndex !== 0" @click="prev" style="font-size: 20px;" name="left"></svg-icon>
     </div>
     <div class="flex-1 relative overflow-hidden">
-      <ul ref="categoryWrapperRef" class="flex w-300% absolute overflow-hidden">
+      <ul ref="categoryWrapperRef" class="flex w-300% absolute overflow-hidden h-100%">
         <li ref="categoryPageRef" class="w-100% " v-for="(item, index) in pages" :key="index">
-      <div  class="flex flex-wrap">
-        <div v-for="(i,index1) in currentItem(index)" :key="index1"  class="w-25% flex flex-col justify-center items-center mb-12px cursor-pointer" >
-           <img  class="w-50% mb-5px" :src="i.pic56x56Url" alt="">
-           <div style="font-size: 12px;">{{ i.name }}</div>
-        </div>
-      </div>
-      
-      </li>
+          <div class="flex flex-wrap h-100%">
+            <div class="flex flex-col justify-between items-center" v-for="(i, index1) in props.row " :key="index1">
+
+              <div class="flex  justify-between items-center h-100%">
+                <div class="flex flex-col justify-center items-center"
+                  v-for="(i2, index3) in currentItem(index).slice((i - 1) * props.column, i * props.column) "
+                  :key="index3">
+                  <img class="w-50% mb-5px" :src="i2.pic56x56Url" alt="">
+                  <div style="font-size: 12px;">{{ i2.name }}</div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </li>
       </ul>
     </div>
     <div class="flex w-20px items-center mx-8px cursor-pointer">
-      <svg-icon v-show="currentIndex!==pages-1" @click="next" style="font-size: 20px;" name="right"></svg-icon>
+      <svg-icon v-show="currentIndex !== pages - 1" @click="next" style="font-size: 20px;" name="right"></svg-icon>
     </div>
   </div>
 </template>
@@ -38,11 +45,13 @@ const props = withDefaults(defineProps<Props>(), {
   row: 2,
   column: 4
 })
-const pages=computed(()=>Math.ceil(props.modelValue.length/(props.row*props.column)) )
-const currentItem=computed(()=>(index:number)=>{
-  const prefix=index*(props.row*props.column)
-  const suffix=(index+1)*(props.row*props.column)
- return props.modelValue.slice(prefix,suffix)
+const pages = computed(() => Math.ceil(props.modelValue.length / (props.row * props.column)))
+const currentItem = computed(() => (index: number) => {
+  const prefix = index * (props.row * props.column)
+  const suffix = (index + 1) * (props.row * props.column)
+
+
+  return props.modelValue.slice(prefix, suffix)
 })
 
 const categoryWrapperRef = ref<HTMLElement>()
@@ -51,28 +60,28 @@ const currentWidth = ref(0)
 const currentIndex = ref(0)
 const prev = () => {
   const width = categoryPageRef.value[0].clientWidth
-  let w=width * currentIndex.value 
-  currentWidth.value = width * (currentIndex.value-1)
+  let w = width * currentIndex.value
+  currentWidth.value = width * (currentIndex.value - 1)
   currentIndex.value -= 1
-  setInterval(()=>{
-    if(w>currentWidth.value){
-      w-=10
+  setInterval(() => {
+    if (w > currentWidth.value) {
+      w -= 10
       categoryWrapperRef.value.style.transform = `translateX(${-w}px)`
     }
-  },10)
+  }, 10)
 }
 
 const next = () => {
   const width = categoryPageRef.value[0].clientWidth
-  let w=width *currentIndex.value 
+  let w = width * currentIndex.value
   currentWidth.value = width * (currentIndex.value + 1)
   currentIndex.value += 1
-  setInterval(()=>{
-    if(w<currentWidth.value){
-      w+=10
+  setInterval(() => {
+    if (w < currentWidth.value) {
+      w += 10
       categoryWrapperRef.value.style.transform = `translateX(${-w}px)`
     }
-  },10)
+  }, 10)
 }
 
 </script>
