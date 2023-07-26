b8162e44b8ee85feab2f9c95d616c22de7c83f07
dshijie <943996999@qq.com>
2023-07-26 15:55:37 +0800

    feat: 视频详情适配mv类型

--- src/api/mv.ts
+++ src/api/mv.ts
@@ -11,3 +11,52 @@ export function getNewMVByAreaApi(params:GetNewMVByAreaParams){
     params
   })
 }
+
+// mv地址
+export function getMvUrlApi(id:number|string){
+  return request({
+    url: '/mv/url',
+    method: 'get',
+    params:{
+      id
+    }
+  })
+}
+// mv详情
+export function getMvDetailApi(mvid:number|string){
+  return request({
+    url: '/mv/detail',
+    method: 'get',
+    params:{
+      mvid
+    }
+  })
+}
+// mv评论
+interface GetMvCommentParams {
+  id:string
+  limit:number
+  offset:number
+  before?:number
+}
+export function getMvCommentApi(params:GetMvCommentParams){
+  return request({
+    url: '/comment/mv',
+    method: 'get',
+    params
+  })
+}
+// 收藏mv
+
+interface SubscribeMvData {
+  mvid:number|string
+  t:1|0
+}
+export function subscribeMvApi(data:SubscribeMvData){
+  return request({
+    url: '/mv/sub',
+    method: 'post',
+    data
+    
+  })
+}
--- src/views/video/videoDetail/videoDetailIndex.vue
+++ src/views/video/videoDetail/videoDetailIndex.vue
@@ -14,18 +14,26 @@
 
           <section v-if="videoInfo.creator" class="flex items-center mt-15px">
             <img class="w-48px h-48px rounded-3xl mr-10px" :src="videoInfo.creator.avatarUrl" alt="">
-            <a class="flex-1" style="font-size: 14px;">{{ videoInfo.creator.nickname }}</a>
+            <a class="flex-1" style="font-size: 14px;">{{ videoInfo.creator.nickname || videoInfo.artistName }}</a>
+            <span style="color: rgb(236, 65, 65);background-color: rgb(52, 44, 43);font-size: 14px;"
+              class="flex items-center py-3px px-15px rounded-2xl cursor-pointer"><svg-icon style="font-size: 24px;"
+                name="plus"></svg-icon>关注 </span>
+          </section>
+
+          <section v-if="videoInfo.artistName" class="flex items-center mt-15px">
+            <img class="w-48px h-48px rounded-3xl mr-10px" :src="videoInfo.artists[0].img1v1Url" alt="">
+            <a class="flex-1" style="font-size: 14px;">{{ videoInfo.artistName }}</a>
             <span style="color: rgb(236, 65, 65);background-color: rgb(52, 44, 43);font-size: 14px;"
               class="flex items-center py-3px px-15px rounded-2xl cursor-pointer"><svg-icon style="font-size: 24px;"
                 name="plus"></svg-icon>关注 </span>
           </section>
 
           <section>
-            <h2 class="p-0 mb-10px mt-30px "> {{ videoInfo.title }}</h2>
+            <h2 class="p-0 mb-10px mt-30px "> {{ videoInfo.title || videoInfo.name }}</h2>
             <div style="color: var(--v-m-text-dark-color);font-size: 14px;" class="mb-10px flex items-center">发布：<span
                 class="mr-30px">{{
                   formatDayTime(videoInfo.publishTime) }}</span>
-              播放：<span>{{ formatCount(videoInfo.playTime)
+              播放：<span>{{ formatCount(videoInfo.playTime) || formatCount(videoInfo.playCount)
               }}</span></div>
             <div>
               <span class="mr-6px py-3px px-8px rounded-2xl" style="font-size: 13px;background-color: rgb(50, 50, 50)"
@@ -38,13 +46,13 @@
           <section class="action-btns flex mt-15px items-center">
             <div @click="likeVideo" style="border:1px solid rgb(75, 75, 75);font-size: 14px;"
               class="cursor-pointer flex items-center px-20px py-6px rounded-2xl mr-10px">
-              <svg-icon class="mr-3px" name="dianzan"></svg-icon> 赞({{ videoInfo.praisedCount
+              <svg-icon class="mr-3px" name="dianzan"></svg-icon> 赞({{ videoInfo.praisedCount || 0
               }})
             </div>
             <div @click="subscribeVideo" style="border:1px solid rgb(75, 75, 75);font-size: 14px;"
               class="cursor-pointer flex items-center px-20px py-6px rounded-2xl mr-10px">
               <svg-icon class="mr-3px" name="collect"></svg-icon>收藏({{
-                videoInfo.subscribeCount }})
+                videoInfo.subscribeCount || videoInfo.subCount }})
             </div>
             <div style="border:1px solid rgb(75, 75, 75);font-size: 14px;"
               class="cursor-pointer flex items-center px-20px py-6px rounded-2xl mr-10px">
@@ -80,6 +88,7 @@
 import { ref, reactive } from 'vue';
 import { useRoute, useRouter } from 'vue-router'
 import { getVideoUrlApi, getVideoDetailApi, getRelatedVideoApi, getVideoCommentApi, likeVideoApi, subscribeVideoApi } from '@/api/video'
+import { getMvUrlApi, getMvDetailApi, getMvCommentApi, subscribeMvApi } from '@/api/mv'
 import { commentApi } from '@/api/comment'
 import { formatCount, formatDayTime } from '@/util';
 import AppComment, { type Comments } from '@/components/Comment/appComment.vue';
@@ -91,18 +100,31 @@ const type = Number(route.query.type)
 const url = ref('')
 const videoRef = ref()
 const getVideoUrl = () => {
-  getVideoUrlApi(String(route.query.id)).then(res => {
-    url.value = res.data.urls[0].url
-  })
+  if (!type) {
+    getVideoUrlApi(String(route.query.id)).then(res => {
+      url.value = res.data.urls[0].url
+    })
+  } else {
+    getMvUrlApi(String(route.query.id)).then(res => {
+      url.value = res.data.data.url
+    })
+  }
 }
 
 getVideoUrl()
 
 const videoInfo = ref<any>({})
 const getVideoDetail = () => {
-  getVideoDetailApi(String(route.query.id)).then(res => {
-    videoInfo.value = res.data.data
-  })
+  if (type) {
+    getMvDetailApi(String(route.query.id)).then(res => {
+      videoInfo.value = res.data.data
+    })
+  } else {
+    getVideoDetailApi(String(route.query.id)).then(res => {
+      videoInfo.value = res.data.data
+    })
+  }
+
 }
 getVideoDetail()
 
@@ -127,18 +149,34 @@ const params = reactive({
   limit: 20
 })
 const getComments = () => {
-  getVideoCommentApi({
-    id: String(route.query.id),
-    offset: (params.offset - 1) * params.limit,
-    limit: params.limit
-  }).then(res => {
-    videoComments.value = []
+  if (type) {
+    getMvCommentApi({
+      id: String(route.query.id),
+      offset: (params.offset - 1) * params.limit,
+      limit: params.limit
+    }).then(res => {
+      videoComments.value = []
 
-    total.value = res.data.total
-    if (res.data.hotComments) {
-      hotComments.value = []
-      res.data.hotComments.slice(0, 9).forEach((item: any) => {
-        hotComments.value.push({
+      total.value = res.data.total
+      if (res.data.hotComments) {
+        hotComments.value = []
+        res.data.hotComments.slice(0, 9).forEach((item: any) => {
+          hotComments.value.push({
+            avatarUrl: item.user.avatarUrl,
+            content: item.content,
+            likeCount: item.likedCount,
+            time: item.time,
+            lieked: item.lieked,
+            nickname: item.user.nickname,
+            commentId: item.commentId,
+            userId: item.user.userId
+          })
+        })
+      }
+
+
+      res.data.comments.forEach((item: any) => {
+        videoComments.value.push({
           avatarUrl: item.user.avatarUrl,
           content: item.content,
           likeCount: item.likedCount,
@@ -149,22 +187,48 @@ const getComments = () => {
           userId: item.user.userId
         })
       })
-    }
+    })
+  } else {
+    getVideoCommentApi({
+      id: String(route.query.id),
+      offset: (params.offset - 1) * params.limit,
+      limit: params.limit
+    }).then(res => {
+      videoComments.value = []
+
+      total.value = res.data.total
+      if (res.data.hotComments) {
+        hotComments.value = []
+        res.data.hotComments.slice(0, 9).forEach((item: any) => {
+          hotComments.value.push({
+            avatarUrl: item.user.avatarUrl,
+            content: item.content,
+            likeCount: item.likedCount,
+            time: item.time,
+            lieked: item.lieked,
+            nickname: item.user.nickname,
+            commentId: item.commentId,
+            userId: item.user.userId
+          })
+        })
+      }
 
 
-    res.data.comments.forEach((item: any) => {
-      videoComments.value.push({
-        avatarUrl: item.user.avatarUrl,
-        content: item.content,
-        likeCount: item.likedCount,
-        time: item.time,
-        lieked: item.lieked,
-        nickname: item.user.nickname,
-        commentId: item.commentId,
-        userId: item.user.userId
+      res.data.comments.forEach((item: any) => {
+        videoComments.value.push({
+          avatarUrl: item.user.avatarUrl,
+          content: item.content,
+          likeCount: item.likedCount,
+          time: item.time,
+          lieked: item.lieked,
+          nickname: item.user.nickname,
+          commentId: item.commentId,
+          userId: item.user.userId
+        })
       })
     })
-  })
+  }
+
 }
 getComments()
 const hanldCurrentChange = (page: number) => {
@@ -196,12 +260,21 @@ const likeVideo = () => {
 }
 
 const subscribeVideo = () => {
-  subscribeVideoApi({ id: String(route.query.id), t: 1 }).then(res => {
-    ElMessage.info(res.data.message)
-    getVideoDetail()
-  }).catch((err) => {
-    ElMessage.info(err.response.data.message)
-  })
+  if (type) {
+    subscribeMvApi({ mvid: String(route.query.id), t: 1 }).then(res => {
+      ElMessage.info(res.data.message)
+      getVideoDetail()
+    }).catch((err) => {
+      ElMessage.info(err.response.data.message)
+    })
+  } else {
+    subscribeVideoApi({ id: String(route.query.id), t: 1 }).then(res => {
+      ElMessage.info(res.data.message)
+      getVideoDetail()
+    }).catch((err) => {
+      ElMessage.info(err.response.data.message)
+    })
+  }
 }
 
 </script>
