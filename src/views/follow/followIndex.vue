ade655acc77492e9cc3605075f762f2d24a4ebfd
dshijie <943996999@qq.com>
2023-07-31 18:08:59 +0800

    feat: 关注页面布局

--- src/views/follow/followIndex.vue
+++ src/views/follow/followIndex.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    follow
+  <div class="flex">
+    <section class="flex-1 px-30px">
+      <section class="fixed-button pt-20px flex items-center fixed bg-#2b2b2b ">
+        <h3 class="p-0 m-0 mr-15px">动态</h3>
+        <span class="flex items-center bg-#ec4141 px-18px py-8px rounded-2xl hover:bg-#d73535 cursor-default">
+          <svg-icon name="send_event" class="mr-3px"></svg-icon>
+          <span style="font-size: 14px;">发动态</span>
+        </span>
+      </section>
+
+      <section class="mt-72px">
+        123333333333333333333333333333333333333333333333333333333333333333333
+      </section>
+    </section>
+
+    <section class="profile w-232px p-30px">2</section>
   </div>
 </template>
 
@@ -8,4 +22,15 @@
 
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.profile {
+  box-shadow: -1px 0px 1px #363636;
+  min-height: calc(100vh - 112px);
+  border-left: 1px solid #363636;
+
+}
+
+.fixed-button {
+  width: calc(100vw - 520px);
+}
+</style>
