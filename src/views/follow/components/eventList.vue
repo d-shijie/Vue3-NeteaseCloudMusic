f5951fb4b61a252b9bef61cdfc80ba64bdacbf36
dshijie <943996999@qq.com>
2023-08-03 18:57:32 +0800

    feat: 用户动态item发布者信息

--- src/views/follow/components/eventList.vue
+++ src/views/follow/components/eventList.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="relative">
-    <div v-if="!events.length" class="absolute start-50% top-50% h-100%">暂无动态</div>
-    <div>111</div>
+    <section v-if="!events.length" class="absolute start-50% top-50% h-100%">暂无动态</section>
+    <section class="mt-88px">
+      <div v-for="(item,index) in events" :key="index" class="flex items-center mb-24px">
+          <img class="w-40px h-40px rounded-full mr-10px cursor-pointer " :src="item.user.avatarUrl" alt="">
+          <span class="flex flex-col justify-between h-40px " style="font-size: 13px;">
+            <div>
+              <span class="mr-10px text-#7098ba hover:text-#98aec1 cursor-pointer " >{{  item.user.nickname}}</span>
+              <span class="text-#8a8a8a">分享声音</span>
+            </div>
+            <div class="text-#8a8a8a" style="font-size: 12px;">
+              {{formateTime( item.eventTime) }}
+            </div>
+          </span>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -9,6 +22,7 @@
 import { ref } from 'vue';
 import {getUserEventApi} from '@/api/user'
 import { useUserStore } from '@/stores/modules/user'
+import { formateTime } from '@/util';
 const userStore = useUserStore()
 const userId = userStore.userInfo.userId
 const events=ref<any[]>([])
