f98006c52bcc5922cf517b5a8b215dcb61373b2a
dshijie <943996999@qq.com>
2023-08-16 14:18:03 +0800

    feat: 个人信息用户创建的歌单默认布局样式

--- src/views/profile/components/createPlaylist.vue
+++ src/views/profile/components/createPlaylist.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
-    createplaylist
+  <div class="flex flex-wrap justify-between">
+    <DefaultPlaylistItem class="w-23%  mb-30px" v-for="(item, index) in playlist" :key="index" :id="item.id"
+      :cover="item.coverImgUrl" :name="item.name" :playCount="item.playCount" :musicCount="item.trackCount" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import { getUserPlaylistApi } from '@/api/user'
+import DefaultPlaylistItem from './modules/defaultPlaylistItem.vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
 
+const playlist = ref<any[]>([])
+const getUserPlaylist = () => {
+  getUserPlaylistApi({
+    uid: Number(route.query.id)
+  }).then(res => {
+
+    playlist.value = res.data.playlist
+
+  })
+}
+getUserPlaylist()
 </script>
 
 <style scoped></style>
--- src/views/profile/components/modules/defaultPlaylistItem.vue
+++ src/views/profile/components/modules/defaultPlaylistItem.vue
@@ -1,11 +1,63 @@
 <template>
   <div>
-    default
+    <section @click="gotoDetail" class="cover relative cursor-pointer">
+      <img class="w-full" :src="props.cover" alt="">
+      <div class="absolute top-5px end-5px flex items-center text-13px">
+        <svg-icon name="play"></svg-icon> {{ props.playCount }}
+      </div>
+      <div class="btn absolute end-10px bottom-10px w-40px h-40px rounded-full bg-#fff flex items-center justify-center">
+        <svg-icon class="text-18px relative left-2px" name="play_red"></svg-icon>
+      </div>
+    </section>
+    <div class="text-15px my-5px">
+      {{ props.name }}
+    </div>
+    <div class="text-13px text-#666666">
+      {{ props.musicCount }}首
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useRouter, useRoute } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+interface Props {
+  id: number
+  cover: string
+  playCount?: number
+  musicCount: number
+  name: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  id: 0,
+  cover: 'https://img1.baidu.com/it/u=950943067,1138707327&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
+  playCount: 0,
+  musicCount: 0,
+  name: ''
+})
 
+const gotoDetail = () => {
+  router.push({
+    path: '/index/playlist-detail',
+    query: {
+      id: Number(props.id)
+    }
+  })
+}
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.cover {
+  .btn {
+    transition: all .5s ease-in-out;
+    opacity: 0;
+  }
+
+  &:hover {
+    .btn {
+      opacity: 1;
+    }
+  }
+}
+</style>
